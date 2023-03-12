58e0b8896d9e3b74f4d8013eb3d36699f6987f69
Sebas19812002 <js.vargasm1@uniandes.edu.co>
2023-03-12 18:51:28 -0500

    Graficas de correlaciones

--- Proyecto_Analitica.py
+++ Proyecto_Analitica.py
@@ -69,7 +69,13 @@ for i in scores:
     #grafica.set_title(i)
     
 plt.show()
-
+#Correlaciones
+for i in scores:
+    for j in scores:
+        fig, ax = plt.subplots()
+        ax.scatter(datos[i],datos[j])
+        ax.set_title (i + " VS " + j)
+        plt.show()
 # Estadisticas descriptivas
 print(datos.min())
 print(datos.mean())
